97d047cdca6fccea2bf84d9af5fb7804e8c1fbec
VollRagm <vollragm@gmail.com>
2022-04-04 00:50:17 +0200

    PTView post

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220404.0014';
+const cacheName = 'chirpy-20220404.0050';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
