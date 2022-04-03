1cf963de93abdec3ba2db1851d407ec07175a81a
VollRagm <vollragm@gmail.com>
2022-04-03 17:59:46 +0200

    Force dark mode

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220403.1743';
+const cacheName = 'chirpy-20220403.1758';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
