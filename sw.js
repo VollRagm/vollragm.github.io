99ecd7021702692db2209982a29321e4ab4fd52a
VollRagm <vollragm@gmail.com>
2022-04-05 01:15:44 +0200

    Unity Engine post

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220404.2318';
+const cacheName = 'chirpy-20220405.0115';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
