92ab120fae4cd317a0c1e275cd3688af98f936f0
VollRagm <vollragm@gmail.com>
2022-04-04 23:19:02 +0200

    Minesweeper post

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220404.0236';
+const cacheName = 'chirpy-20220404.2318';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
