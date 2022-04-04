978e81b82b4cc2911c0920c8e7a2e32ed63a0f1d
VollRagm <vollragm@gmail.com>
2022-04-04 02:17:35 +0200

    Fixed creation date

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220404.0213';
+const cacheName = 'chirpy-20220404.0217';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
