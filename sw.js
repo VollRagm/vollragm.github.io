be4971e65235016a6cea5698dd96980038e42e3b
VollRagm <vollragm@gmail.com>
2022-04-04 02:37:04 +0200

    Added discord

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220404.0217';
+const cacheName = 'chirpy-20220404.0236';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
