00d678a8a510f2330b5b36a78f52f945f252b263
VollRagm <vollragm@gmail.com>
2022-04-05 16:50:34 +0200

    Kernel msg box post

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220405.0203';
+const cacheName = 'chirpy-20220405.1650';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
