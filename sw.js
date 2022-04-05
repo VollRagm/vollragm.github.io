a1e411069d2946ebdec5164564d329a5877054c1
VollRagm <vollragm@gmail.com>
2022-04-05 17:17:18 +0200

    yet another mistake fixed..

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220405.1714';
+const cacheName = 'chirpy-20220405.1716';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
