4a3a1e828689622666334717184e926ea504e55d
VollRagm <vollragm@gmail.com>
2022-04-05 18:14:31 +0200

    updated

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220405.1806';
+const cacheName = 'chirpy-20220405.1808';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
