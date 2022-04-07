4f701b32dabd0f59e9791de9a0c702e20a8fa4f0
VollRagm <vollragm@gmail.com>
2022-04-07 22:54:44 +0200

    added song for the raccoon

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220405.2114';
+const cacheName = 'chirpy-20220407.2254';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
