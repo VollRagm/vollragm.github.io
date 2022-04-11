c3d1cf20e9ba0bd258306d64a86a68cc08064040
VollRagm <vollragm@gmail.com>
2022-04-11 17:12:33 +0200

    Fixed typo

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220411.0127';
+const cacheName = 'chirpy-20220411.1712';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
