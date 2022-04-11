13122a6048a32cfe5044abbbc537c0c54fb85326
VollRagm <vollragm@gmail.com>
2022-04-11 20:16:12 +0200

    Fixed another mistake

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220411.1712';
+const cacheName = 'chirpy-20220411.2015';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
